2b00757560889594f19d03f0c3c0bff79b1492f2
crocodilered <Preferences11>
2019-09-25 10:32:10 +0300

    Models fields come to reality.

--- organization/models.py
+++ organization/models.py
@@ -5,22 +5,40 @@ class Employee(models.Model):
     """
     Работник КСО
     """
+
+    # Ссылка на организацию
     organization_kso = models.ForeignKey(
         'OrganizationKso',
         on_delete=models.CASCADE,
         related_name='employees'
     )
-    name = models.CharField(max_length=50)
+
+    # Фамилия, имя, отчество
+    name_last = models.CharField(max_length=20)
+    name_first = models.CharField(max_length=20)
+    name_second = models.CharField(max_length=20)
+
+    # Должность, отдел
+    department = models.CharField(max_length=30)
+    position = models.CharField(max_length=30)
+
+    # Телефон, эл. почта
+    phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
 
     def __str__(self):
-        return '{} - {}'.format(self.name, self.organization_kso)
+        return '{} {} {}'.format(self.name_last, self.name_first, self.name_second)
 
 
 class OrganizationKso(models.Model):
     """
     Организация - КСО
     """
-    title = models.CharField(max_length=200)                # Название
+
+    class Meta:
+        ordering = ['title']
+
+    # Ссылка на справочник ЕГРЮЛ
     organization_common = models.OneToOneField(
         'OrganizationCommon',
         on_delete=models.SET_NULL,
@@ -28,25 +46,56 @@ class OrganizationKso(models.Model):
         null=True
     )
 
+    # Название
+    title = models.CharField(max_length=200)
+
     def __str__(self):
         return '{}'.format(self.title)
-
-    class Meta:
-        ordering = ['title']
 
 
 class OrganizationCommon(models.Model):
     """
-    Организация из списка ЕГРЮЛ
+    Справочник ЕГРЮЛ
     """
-    title = models.CharField(max_length=200)    # Название
-    ogrn = models.CharField(max_length=13)      # ОГРН
-    inn = models.CharField(max_length=12)       # ИНН
-
-    def __str__(self):
-        return '{}'.format(self.title)
 
     class Meta:
-        ordering = ['title']
+        ordering = ['title_full']
 
+    # Дата регистрации
+    reg_date = models.DateField()
 
+    # Наименование ОПФ
+    opf_full = models.CharField(max_length=50)
+    opf_short = models.CharField(max_length=5)
+
+    # Наименование ЮЛ
+    title_full = models.CharField(max_length=200)
+    title_short = models.CharField(max_length=100)
+
+    # ИНН, КПП, ОГРН
+    inn = models.CharField(max_length=12)
+    kpp = models.CharField(max_length=9)
+    ogrn = models.CharField(max_length=13)
+
+    # Адрес
+    addr_index = models.CharField(max_length=6)
+    addr_region = models.CharField(max_length=20)
+    addr_locality = models.CharField(max_length=20)
+    addr_street = models.CharField(max_length=20)
+    addr_building = models.CharField(max_length=5)
+    addr_housing = models.CharField(max_length=5)
+    addr_office = models.CharField(max_length=5)
+
+    # Руководитель
+    head_position = models.CharField(max_length=20)
+    head_name_last = models.CharField(max_length=20)
+    head_name_first = models.CharField(max_length=20)
+    head_name_second = models.CharField(max_length=20)
+    head_accession_date = models.DateField()
+
+    # Служебные
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.inn, self.title_full)
