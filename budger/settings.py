26629d3ec7a392f3aa3e15a8c908388a053bc953
crocodilered <Preferences11>
2019-10-11 16:01:48 +0300

    Timezone fixed.

--- budger/settings.py
+++ budger/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
-TIME_ZONE = 'UTC+3'
+TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
