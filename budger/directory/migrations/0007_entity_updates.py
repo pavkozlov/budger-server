8b5bb2d647f1d245f80921ed5323c7e7afb6b725
crocodilered <Preferences11>
2019-10-25 16:29:15 +0300

    Conflict merged.

--- budger/directory/migrations/0007_entity_updates.py
+++ budger/directory/migrations/0007_entity_updates.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('directory', '0006_auto_20191022_1523'),
+        ('directory', '0007_auto_20191024_1707'),
     ]
 
     operations = [
