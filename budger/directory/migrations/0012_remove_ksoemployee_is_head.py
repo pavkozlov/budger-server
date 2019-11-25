7daff0ed8465fff1a12b5f87fd18e4ff8dd1634b
crocodilered <Preferences11>
2019-11-25 12:28:46 +0300

    Merge migrations.

--- budger/directory/migrations/0012_remove_ksoemployee_is_head.py
+++ budger/directory/migrations/0012_remove_ksoemployee_is_head.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('directory', '0011_kso_head'),
+        ('directory', '0012_ksodepartment2_head'),
     ]
 
     operations = [
