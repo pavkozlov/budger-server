0bb1b90e2b2107302b52fda18a7f514d6e65bd26
crocodilered <Preferences11>
2019-12-04 16:44:24 +0300

    Fixed a little.

--- budger/directory/signals.py
+++ budger/directory/signals.py
@@ -5,7 +5,7 @@ from .models.kso import KsoEmployee
 
 @receiver(post_save, sender=KsoEmployee)
 def update_user_with_employee(sender, instance, *args, **kwargs):
-    if instance and instance.user is not None:
+    if instance and instance.user is not None and instance.user.email is not None:
         user = instance.user
         user.email = instance.email.lower()
         user.username = user.email.split('@')[0].lower()
