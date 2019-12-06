a0ab27ef8593fc1c9275a45796ec80811782058c
crocodilered <Preferences11>
2019-12-06 15:57:15 +0300

    Complex filter done.

--- budger/directory/filters.py
+++ budger/directory/filters.py
@@ -1,4 +1,5 @@
 from rest_framework import filters
+from django.db.models import Q
 
 
 class EntityFilter(filters.BaseFilterBackend):
@@ -11,5 +12,14 @@ class EntityFilter(filters.BaseFilterBackend):
         if request.query_params.get('_filter__inn'):
             term = request.query_params.get('_filter__inn')
             return queryset.filter(inn__contains=term)
+
+        if request.query_params.get('_complex_filter_1'):
+            term = request.query_params.get('_complex_filter_1')
+            return queryset.filter(
+                Q(title_search__icontains=term) |
+                Q(inn__contains=term) |
+                Q(ogrn__contains=term) |
+                Q(head_name__icontains=term)
+            )
 
         return queryset.all()
