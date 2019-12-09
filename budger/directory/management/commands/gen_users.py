73e1e31dcd8e193395927a6be02106ca56a0bdce
crocodilered <Preferences11>
2019-12-09 14:58:56 +0300

    Fixed

--- budger/directory/management/commands/gen_users.py
+++ budger/directory/management/commands/gen_users.py
@@ -10,23 +10,8 @@ class Command(BaseCommand):
         employees = KsoEmployee.objects.filter(kso_id=1)
 
         for employee in employees:
-            if employee.email not in [None, ''] and '@' in employee.email:
-                print('{} ... '.format(employee.name), end='')
-
-                data = {
-                    'username': employee.email.split('@')[0].lower(),
-                    'email': employee.email.lower(),
-                    'password': 'admin'
-                }
-
-                try:
-                    user = User.objects.get(email=data['email'])
-                    print(user, 'applied.')
-                except User.DoesNotExist:
-                    user = User.objects.create_user(data)
-                    print('created.')
-
-                employee.user = user
-                employee.save()
-
-
+            user = employee.user
+            if user:
+                print(user.username)
+                user.set_password('admin')
+                user.save()
