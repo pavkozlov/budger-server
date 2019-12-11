68ae6a7b2b9dba8c7d99fea147699f5e34f8b513
pkozlov <it.pavelkozlov@gmail.com>
2019-12-11 16:41:14 +0300

    Title search for entity fix.

--- budger/triggers.sql
+++ budger/triggers.sql
@@ -3,8 +3,17 @@
 DROP FUNCTION IF EXISTS entity_save CASCADE;
 
 CREATE FUNCTION entity_save() RETURNS trigger AS $entity_save$
+    DECLARE
+    	title_full VARCHAR(2000);
+		title_short VARCHAR(2000);
+		title_search VARCHAR(4001);
+
     BEGIN
-        NEW.title_search := UPPER(CONCAT(NEW.title_full, ' ', NEW.title_short));
+        title_full := REGEXP_REPLACE(NEW.title_full, '[^\w\d\s]+', ' ', 'g');
+        title_short := REGEXP_REPLACE(NEW.title_short, '[^\w\d\s]+', ' ', 'g');
+        title_search := UPPER(CONCAT(title_full, ' ', title_short));
+        title_search := REGEXP_REPLACE(title_search, '\s+', ' ', 'g');
+        NEW.title_search := title_search;
         RETURN NEW;
     END;
 $entity_save$ LANGUAGE plpgsql;
@@ -19,7 +28,7 @@ DROP FUNCTION IF EXISTS kso_save CASCADE;
 CREATE FUNCTION kso_save() RETURNS trigger AS $kso_save$
 
 	DECLARE
-    	title_full              VARCHAR(1000);
+    	title_full VARCHAR(1000);
 
     BEGIN
     	IF NEW.entity_id IS NOT NULL THEN
