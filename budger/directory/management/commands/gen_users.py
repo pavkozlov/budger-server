32c4ca8bc431803ec91b7ff49461d07d2ee2d3d2
pkozlov <it.pavelkozlov@gmail.com>
2019-12-16 17:07:20 +0300

    Password generator for users added.

--- budger/directory/management/commands/gen_users.py
+++ budger/directory/management/commands/gen_users.py
@@ -1,17 +1,39 @@
 from django.core.management.base import BaseCommand
 from budger.directory.models.kso import KsoEmployee
 from django.contrib.auth.models import User
+import random
+import string
+
+STAFF = ['KorolihinVV@mosreg.ru', 'admin@admin.ru']
+
+
+def generate_password(password_len=8):
+    chars = string.digits + string.ascii_letters
+    password_list = random.choices(chars, k=password_len)
+    return ''.join(password_list)
 
 
 class Command(BaseCommand):
-    help = 'Script for creating users for KsoEmployees'
+    help = 'Script for creating users for KsoEmployees or change password if user exists'
 
     def handle(self, *args, **options):
         employees = KsoEmployee.objects.filter(kso_id=1)
 
         for employee in employees:
-            user = employee.user
-            if user:
-                print(user.username)
-                user.set_password('admin')
-                user.save()
+
+            if employee.email in [None, '', *STAFF] or '@' not in employee.email:
+                continue
+
+            email = employee.email.lower()
+            username = email.split('@')[0]
+            password = generate_password()
+
+            if employee.user is None:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                employee.user = user
+                employee.save()
+            else:
+                employee.user.set_password(password)
+                employee.user.save()
+
+            print('{} {}'.format(email, password))
