d136bba7d8cbae6a2f9e96ab14bc0f0867076465
crocodilered <Preferences11>
2019-12-19 11:47:33 +0300

    Remove unused serializer.

--- budger/bubbles/serializers.py
+++ budger/bubbles/serializers.py
@@ -1,8 +1,3 @@
 from rest_framework import serializers
 
 
-class NatProjectShortSerialiser(serializers.Serializer):
-    id = serializers.IntegerField()
-    title_full = serializers.CharField(max_length=300)
-    curator = serializers.CharField(max_length=300)
-    responsible = serializers.CharField(max_length=300)
