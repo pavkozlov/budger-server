3f1ced00d4c157a19429120ca1040f34fce98e43
crocodilered <Preferences11>
2019-12-20 10:44:06 +0300

    Add XML renderer (frame) for ESGFK export.

---
+++ budger/schedules/renderers.py
@@ -0,0 +1,19 @@
+from rest_framework import renderers
+
+
+class EsgfkXmlRenderer(renderers.BaseRenderer):
+    media_type = 'application/xml'
+    format = '.xml'
+
+    @staticmethod
+    def _render_model(model):
+        xml = f'<title>{model.title}</title>'
+        return f'<model>{xml}</model>'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        xml = ''
+
+        for model in data:
+            xml += self._render_model(model)
+
+        return '<?xml version="1.0"?><root>{}</root>'.format(xml)
--- budger/schedules/urls.py
+++ budger/schedules/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, EnumsApiView, WorkflowViewSet
+from .views import EventViewSet, EnumsApiView, WorkflowViewSet, EsgfkXmlView
 
 router = DefaultRouter()
 router.register('events', EventViewSet, basename='Event')
@@ -8,5 +8,6 @@ router.register('workflows', WorkflowViewSet, basename='Workflow')
 
 urlpatterns = [
     path('_enums/', EnumsApiView.as_view()),
+    path('eisgfk-xml/', EsgfkXmlView.as_view()),
     path('', include(router.urls)),
 ]
